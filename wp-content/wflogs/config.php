5ab862a97936e0c2eac67f3fa488e813cf652c69
gabrielrey <gabrielrey0102@gmail.com>
2022-06-27 07:21:25 -0400

    Wordpress

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1656428764;s:7:"authKey";s:64:"9TjPgz#HhxJc]J/uJKL{jYcP0F~^7]5yk0[d2TOd -g]] ?T$;ffYEUyq)4^!G=|";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3775;}
